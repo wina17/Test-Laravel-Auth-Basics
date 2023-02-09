cf1b923c5a75f2ee52d4bc041d80fdc7550ff30d
wina17 <wina17_mcbgroup@yahoo.com>
2023-02-09 17:42:06 +0700

    Testing auth

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -29,7 +29,8 @@
                                      class="block mt-1 w-full"
                                      type="text"
                                      name="name"
-                                     value="???"
+                                     value="{{ auth()->user()->name }}"
+                                     {{-- value="{{ Auth::user()->name }}" --}}
                                      required />
                         </div>
 
@@ -40,7 +41,8 @@
                                      class="block mt-1 w-full"
                                      type="email"
                                      name="email"
-                                     value="???"
+                                     value="{{ auth()->user()->email }}"
+                                     {{-- value="{{ Auth::user()->email }}" --}}
                                      required />
                         </div>
 
